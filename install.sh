2ccb75b5f6d14b79d65e2c8a17be795176206dab
DavidHerreros <david.herreroscalero@gmail.com>
2025-01-16 14:42:09 +0100

    Fix typo

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ pip install pynvml packaging
 colored_echo "green" "...Done"
 
 # Run Conda installation
-if python tensorflow_toolkit/build.py --condaBin $CONDABIN ; then
+if python tensorflow_toolkit/build.py --condaBin "$CONDABIN" ; then
   colored_echo "green" "Environment: flexutils-tensorflow built succesfully"
 else
   colored_echo "red" "Error when building flexutils-tensorflow - Exiting"
@@ -117,6 +117,6 @@ if is_python_package_installed $python_package; then
   colored_echo "green" "Open3D package is already installed. Skipping..."
 else
   colored_echo "green" "Installing Open3D..."
-  bash ./install_open3d.sh --condaBin $CONDABIN
+  bash ./install_open3d.sh --condaBin "$CONDABIN"
   colored_echo "green" "Done..."
 fi
